eb87a106719dbbd69412c97e3e86b0853a4be667
Kevin-Conaghan <kevin_conaghan@hotmail.co.uk>
2020-10-11 21:07:29 +0100

    Adding Dev folder
    -Removing Library artifacts
    -Adding character movement

---
+++ GJLGameJam2020/Assets/Kevin/Scritps/Camera_Follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public float cameraDistance;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraDistance = -cameraDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y - cameraDistance, playerTransform.position.z);
+    }
+}
---
+++ GJLGameJam2020/Assets/Kevin/Scritps/Character_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(moveHorizontal, rb.velocity.y, moveVertical);
+        transform.LookAt(transform.position + new Vector3(movement.x, 0.0f, movement.z));
+
+        rb.velocity = movement;
+    }
+}
